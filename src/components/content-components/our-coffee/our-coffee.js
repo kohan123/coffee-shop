1f3cbff3294a36945f573bacdfb7bf7a8a937419
kohan123 <sportodavka@gmail.com>
2023-06-06 19:06:48 +0200

    feat: add second page

--- src/components/content-components/our-coffee/our-coffee.js
+++ src/components/content-components/our-coffee/our-coffee.js
@@ -1,9 +1,12 @@
 import './our-coffee.css';
 
+import Header from '../../header/header';
+import  headerBg2 from '../../../images/header/header2.png';
+
 const OurCoffee = () => {
     return (
         <div>
-
+            <Header img={headerBg2} height={'250px'} title='Our coffee'/>
         </div>
     )
 }
